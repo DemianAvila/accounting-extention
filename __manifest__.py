bef4960c140b8d10255dc79e52411272ece640f3
demianavilar <demianavilar@demianavilar.arinnovations.com.mx>
2024-10-09 17:42:38 -0600

    feat: added client to display table

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "accounting-extention",
+
+    'summary': "Module that goes in line with Integritas tech test",
+
+    'description': """
+    This module extends Accounting module to insert tables
+    """,
+
+    'author': "Integritas - Demian Avila",
+    'website': "-",
+
+    'category': 'Accounting',
+    'version': '0.1',
+
+    'depends': ['base', 'account'],
+
+    'data': [
+        # 'security/ir.model.access.csv',
+        #'views/views.xml',
+        #'views/templates.xml',
+    ],
+    
+}
+
